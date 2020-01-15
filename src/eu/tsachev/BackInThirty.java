9a551d97e4cf57757e4a52503bfc0b59c690dca3
Vla-Dun <56652262+Vla-Dun@users.noreply.github.com>
2020-01-15 18:44:05 +0200

    funda java 2020

---
+++ src/eu/tsachev/BackInThirty.java
@@ -0,0 +1,27 @@
+package eu.tsachev;
+
+import java.util.Scanner;
+
+public class BackInThirty {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int hour = Integer.parseInt(scan.nextLine()), min = Integer.parseInt(scan.nextLine()) + 30;
+
+
+        if (min > 59) {
+
+            hour += 1;
+            if (hour > 23) {
+                hour = 0;
+            }
+            min -= 60;
+        }
+        if (min < 10) {
+            System.out.printf("%d:%02d", hour, min);
+        } else {
+            System.out.printf("%d:%d", hour, min);
+        }
+    }
+}
+
