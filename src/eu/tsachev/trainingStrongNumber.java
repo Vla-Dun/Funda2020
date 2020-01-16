88369cbafcfe674cf33e0dd2093904445e9e7ec3
Vla-Dun <56652262+Vla-Dun@users.noreply.github.com>
2020-01-16 20:25:30 +0200

    funda java 2020

---
+++ src/eu/tsachev/trainingStrongNumber.java
@@ -0,0 +1,33 @@
+package eu.tsachev;
+
+import java.util.Scanner;
+
+public class trainingStrongNumber {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+        int num = Integer.parseInt(scan.nextLine());
+        int i, f, r, sum = 0, temp;
+        temp = num;
+
+        while (num != 0) {
+            i = 1;
+            f = 1;
+            r = num % 10;
+            while (i <= r) {
+                f = f * i;
+                i++;
+            }
+            sum = sum + f;
+            num = num / 10;
+        }
+
+        if (sum == temp) {
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
+        }
+
+    }
+}
---
+++ src/eu/tsachev/triangle.java
@@ -0,0 +1,18 @@
+package eu.tsachev;
+
+import java.util.Scanner;
+
+public class triangle {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        Integer n=Integer.parseInt(scan.nextLine());
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <i ; j++) {
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+}
