2a426ef80f56d4a8cbbf091a8a9cc3a86737ed3e
Vla-Dun <56652262+Vla-Dun@users.noreply.github.com>
2020-02-20 10:11:07 +0200

    funda java 2020

--- src/eu/tsachev/arrays/arrayCommomElements.java
+++ src/eu/tsachev/arrays/arrayCommomElements.java
@@ -2,7 +2,7 @@ package eu.tsachev.arrays;
 
 import java.util.Scanner;
 
-public class arrayCommomElements {
+public class  arrayCommomElements {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
