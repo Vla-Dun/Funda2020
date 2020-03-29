193f81d515061fd6f1f7cde0e91ca9a73fe9d9b5
Vla-Dun <56652262+Vla-Dun@users.noreply.github.com>
2020-03-29 11:53:02 +0300

    funda java 2020

--- src/eu/tsachev/examprep/charityMeals.java
+++ src/eu/tsachev/examprep/charityMeals.java
@@ -6,16 +6,17 @@ public class charityMeals {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        Map<String, List<String>> liked = new HashMap<>();
+        Map<String, List<String>> liked = new TreeMap<>();
         String line = scan.nextLine();
+        int unlikedMeals = 0;
+
         while (!"Stop".equals(line)) {
             String[] split = line.split("-");
             String type = split[0];
             String guestName = split[1];
             String mealName = split[2];
 
-            if ("Like".equals(type)
-            ) {
+            if ("Like".equals(type)) {
 
                 List<String> guestMeals = liked.get(guestName);
 
@@ -30,20 +31,38 @@ public class charityMeals {
                 liked.put(guestName, guestMeals);
 
             } else {
-
-                List<String> meals = liked.get(guestName);
-                if (!meals.contains(mealName)) {
-                    System.out.printf("%s doesn't have the %s in his/her collection.%n", guestName, mealName);
+                if (!liked.containsKey(guestName)) {
+                    System.out.println(guestName + " is not at the party.");
                 } else {
-                    meals.remove(mealName);
-                    System.out.printf("%s doesn't like the %s.%n");
+
+                    List<String> meals = liked.get(guestName);
+
+                    if (!meals.contains(mealName)) {
+                        System.out.printf("%s doesn't have the %s in his/her collection.%n", guestName, mealName);
+                    } else {
+                        unlikedMeals++;
+                        meals.remove(mealName);
+                        System.out.printf("%s doesn't like the %s.%n", guestName, mealName);
+                    }
                 }
-
             }
-
-
             line = scan.nextLine();
-        }//while
+        }
+
+
+        liked
+                .entrySet()
+                .stream()
+                .sorted((l, r) -> r.getValue().size() - l.getValue().size())
+                .forEach(s -> System.out.println(s.getKey() + ": " + printList(s.getValue())));
+
+
+        System.out.println("Unliked meals: " + unlikedMeals);
+
+    }
+
+    private static String printList(List<String> value) {
+        return value.toString().replaceAll("[\\[\\]]", "");
     }
 }
 
